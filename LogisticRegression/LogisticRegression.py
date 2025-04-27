afe272ce1cc1f9026e32cea7b18365833a88f937
bisbist <bistbishal69@gmail.com>
2025-04-27 11:51:32 +0545

    initial commit

---
+++ LogisticRegression/LogisticRegression.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class LogisticRegression():
+    def hello():
+        print("Hello from Logistic Regression") 
---
+++ LogisticRegression/train.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from LogisticRegression import LogisticRegression
+
+print("Hello from Logistic Regression train.py")
